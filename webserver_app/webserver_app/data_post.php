9b26beabfba840e7785e5cc55eecc00d89eb291e
Nguyễn Cao Kỳ <Caoky.nguyen.1704@gmail.com>
2020-10-28 14:03:13 +0700

    update code & database

--- webserver_app/webserver_app/data_post.php
+++ webserver_app/webserver_app/data_post.php
@@ -12,14 +12,20 @@
 			$stmt->execute(array(':xacthuc' => md5($_POST['sign'])));
 			$rows = $stmt->fetchAll();
 			if (count($rows)==1){
+				$sql = 'SELECT * FROM device WHERE sign = :xacthuc';
+				$stmt = $pdo->prepare($sql);
+				$stmt->execute(array(':xacthuc' => md5($data['sign'])));
+				$rows = $stmt->fetchAll();
+				$device_id=$rows[0]['device_id'];
 				if (((isset($_POST['nhietdo'])))&&(isset($_POST['doam']))){
 					date_default_timezone_set('Asia/Ho_Chi_Minh');
-					$sql = "INSERT INTO data (date_post, nhietdo, doam) VALUES (:datepost, :nhietdo, :doam)";
+					$sql = "INSERT INTO data (date_post, nhietdo, doam) VALUES (:datepost, :nhietdo, :doam, :deviceid)";
 					$stmt = $pdo->prepare($sql);
 					$stmt->execute(array(
 						':datepost' => date("d/m/Y H:i"),
 						':nhietdo' => $_POST['nhietdo'],
-						':doam' => $_POST['doam']));
+						':doam' => $_POST['doam'],
+						':device_id'=>$device_id));
 					$response->status = 1;
 					$response->msg = "Thêm thành công";
 				}else{
@@ -42,14 +48,20 @@
 					$stmt->execute(array(':xacthuc' => md5($data['sign'])));
 					$rows = $stmt->fetchAll();
 					if (count($rows)==1){
+						$sql = 'SELECT * FROM device WHERE sign = :xacthuc';
+						$stmt = $pdo->prepare($sql);
+						$stmt->execute(array(':xacthuc' => md5($data['sign'])));
+						$rows = $stmt->fetchAll();
+						$device_id=$rows[0]['device_id'];
 						if (((isset($data['nhietdo'])))&&(isset($data['doam']))){
 							date_default_timezone_set('Asia/Ho_Chi_Minh');
-							$sql = "INSERT INTO data (date_post, nhietdo, doam) VALUES (:datepost, :nhietdo, :doam)";
+							$sql = "INSERT INTO data (date_post, nhietdo, doam, device_id) VALUES (:datepost, :nhietdo, :doam, :deviceid)";
 							$stmt = $pdo->prepare($sql);
 							$stmt->execute(array(
 								':datepost' => date("d/m/Y H:i"),
 								':nhietdo' => $data['nhietdo'],
-								':doam' => $data['doam']));
+								':doam' => $data['doam'],
+								':deviceid'=>$device_id));
 							$response->status = 1;
 							$response->msg = "Thêm thành công";
 						}else{
